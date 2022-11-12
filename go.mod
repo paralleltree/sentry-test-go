d3e68a3492d5d835fc122ce701104637721bb0de
Paralleltree <paralleltree@outlook.com>
2022-11-12 16:34:09 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module rollbartest
+
+go 1.19
+
+require (
+	github.com/getsentry/sentry-go v0.15.0 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+	"golang.org/x/xerrors"
+)
+
+func main() {
+	err := sentry.Init(sentry.ClientOptions{
+		// Set TracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production,
+		TracesSampleRate: 1.0,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	defer sentry.Flush(2 * time.Second)
+
+	sentry.CaptureMessage("It works!")
+	if err := doSomething1(); err != nil {
+		sentry.CaptureException(err)
+	}
+}
+
+func doSomething1() error {
+	if err := doSomething2(); err != nil {
+		return xerrors.Errorf("doSomething1: %w", err)
+	}
+	return nil
+}
+
+func doSomething2() error {
+	if err := somethingWentWrong(); err != nil {
+		return xerrors.Errorf("doSomething2: %w", err)
+	}
+	return nil
+}
+
+func somethingWentWrong() error {
+	err := errors.New("internal error: param1: b")
+	return xerrors.Errorf("oops: %w", err)
+}
